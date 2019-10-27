325c64fd1ff70297e501bd48c0944d5c7e8c4210
phawit-s <56498877+phawit-s@users.noreply.github.com>
2019-10-27 11:51:31 +0700

    Delete dbconnect.php

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$hostName="localhost";
-$user="root";
-$pass="";
-$dbName="product";
-$connect=mysqli_connect($hostName, $user, $pass, $dbName) or die("Can't connect")
-echo"Connection succesful"
-mysqli_set_charset($connect,"utf8");
