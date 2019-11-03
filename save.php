e203b7eef9f7578f84d543329387972c8255167f
phawit-s <56498877+phawit-s@users.noreply.github.com>
2019-11-03 15:41:08 +0700

    Add files via upload

---
+++ save.php
@@ -0,0 +1,41 @@
+<?PHP
+	// Database Connection code
+	$host = 'localhost';
+	$username = 'id11387127_projectpsit2019';
+	$password = 'Imnayeon1995';
+	$databasename = 'id11387127_psit';
+    $con = mysqli_connect($host,$username,$password,$databasename);
+    if(isset($_GET['save'])) {
+
+    $userid = $_GET['userid'];
+    $Name = $_GET['Name'];
+    $Surname = $_GET['Surname'];
+	$email = $_GET['email'];
+    $gender = $_GET['gender'];
+    $userwname = $_GET['userwname'];
+	$pass1 = $_GET['renterPass'];
+    $pass2 = $_GET['rconfirmPass'];
+    }
+
+	if(!$con)
+	{
+		die("Error : ".mysqli_connect_error());
+	}
+	if($pass1 == $pass2)
+	{
+		$sql = "INSERT INTO `register`(`userid`, `Name`, `Surname`, `email`, `userwname`, `pass`, `gender`) VALUES('$userid', '$Name', '$Surname', '$email', '$userwname', '$pass1', '$gender');";
+		if(mysqli_query($con,$sql))
+		{
+			echo "สมัครเสร็จสิ้น";
+		}
+		else
+		{
+			echo "เกิดข้อผิดพลาด";
+		}
+	}else
+	{
+		echo "รหัสไม่ถูกต้อง";
+	}
+	
+	mysqli_close($con);
+?>
