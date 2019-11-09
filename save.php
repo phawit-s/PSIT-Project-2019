5139538585b4dd4efa41f78d89bef84461ff72d6
phawit-s <56498877+phawit-s@users.noreply.github.com>
2019-11-09 13:36:38 +0700

    Update save.php

--- save.php
+++ save.php
@@ -6,39 +6,49 @@
 	$databasename = 'id11387127_psit';
     $con = mysqli_connect($host,$username,$password,$databasename);
     if(isset($_GET['save'])) {
-
+    
+    // GET information from register.html
     $userid = $_GET['userid'];
     $Name = $_GET['Name'];
     $Surname = $_GET['Surname'];
-    $email = $_GET['email'];
+	$email = $_GET['email'];
     $gender = $_GET['gender'];
     $userwname = $_GET['userwname'];
-    $pass1 = $_GET['renterPass'];
+	$pass1 = $_GET['renterPass'];
     $pass2 = $_GET['rconfirmPass'];
     }
-
+    // if connection fail or not
 	if(!$con)
 	{
 		die("Error : ".mysqli_connect_error());
-	}
+    }
+    // if password and comfirmpassword have the same word.
 	if($pass1 == $pass2)
 	{
+        // insert data from register.html into databases
 		$sql = "INSERT INTO `register`(`userid`, `Name`, `Surname`, `email`, `userwname`, `pass`, `gender`) VALUES('$userid', '$Name', '$Surname', '$email', '$userwname', '$pass1', '$gender');";
 		if(mysqli_query($con,$sql))
 		{
-			echo "สมัครเสร็จสิ้น";
+			echo "<script>
+            alert('Successful');
+            window.location.href='login.html';
+            </script>";
 		}
 		else
 		{
-			echo "เกิดข้อผิดพลาด";
+			echo "<script>
+            alert('ERROR');
+            window.location.href='index.html';
+            </script>";
 		}
 	}else
 	{
-		echo "<script>
-    		alert('Wrong password');
-    		window.location.href='https://projectpsit2019.000webhostapp.com/';
-    		</script>";
+        // if  comfirm password has not the same password with password
+        echo "<script>
+    alert('Wrong password');
+    window.location.href='index.html';
+    </script>";
 	}
-	
+	// close connection
 	mysqli_close($con);
 ?>
