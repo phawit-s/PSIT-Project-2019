b85ce048dd48958bf0b992103d826c47bde9c12a
phawit-s <56498877+phawit-s@users.noreply.github.com>
2019-11-16 15:54:17 +0700

    Update profile.php

--- profile.php
+++ profile.php
@@ -10,6 +10,7 @@
           <li><a href="logout.php">logout</a></li>
           <li><a href="#">WELCOME <?php echo $_SESSION['Name']; ?> <?php echo $_SESSION['Surname']; ?></a></li>
           <li><a href="contact page.php">Contact Us</a></li>
+          <li><a href="index.php">Home</a></li>
         <?php }else { ?>
                 <li><a href="login.html">login</a></li>
                 <li><a href="register.html">register</a></li>
@@ -18,13 +19,13 @@
     </div>
     <?php if(isset($_SESSION['Name'])) { ?>
     <ul>
-        <?php echo '<img src="data:image/jpeg;base64,'.base64_encode($_SESSION['picture']).'"/>'; ?>
+        <li><?php echo $_SESSION['picture']; ?></li>
         <li><?php echo $_SESSION['userwname']; ?></li>
         <li><?php echo $_SESSION['Name']; ?></li>
         <li><?php echo $_SESSION['Surname']; ?></li>
         <li><?php echo $_SESSION['email']; ?></li>
-        <li><?php echo $_SESSION['gender']; ?></li>
-        <a href="edit.php">edit</a>
+        <li><?php echo $_SESSION['gender']; ?></li>  
+
 
     <?php }else { ?>
     <?php } ?>
