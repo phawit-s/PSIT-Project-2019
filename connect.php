1b7582355f51f9138d9b3b6c5767386faeb52029
phawit-s <56498877+phawit-s@users.noreply.github.com>
2019-11-16 19:56:10 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = new mysqli('localhost', 'id11387127_projectpsit2019', 'Imnayeon1995', 'id11387127_psit');
+    $conn = new mysqli('localhost', 'id11387127_projectpsit2019', '*********', 'id11387127_psit');
     if($conn->connect_errno){
         die("Connection fail".$conn->connect_errno);
     }
