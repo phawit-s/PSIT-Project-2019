deb7ca427ae762b6d5694dabe3972c9204565db3
phawit-s <phawit.ps@gmail.com>
2019-11-21 22:44:32 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,5 @@
 <?php
     $db_connect=new mysqli('localhost', 'id11387127_projectpsit2019', 'Imnayeon1995', 'id11387127_psit');
-    if($db_connect->connect_errno==true)
-        echo "connection fail";
-    else
-        echo "Success";
+
     $db_connect->close();
 ?>
