df1f94c52229abbdf399bafa6508c318e0843947
phawit-s <phawit.ps@gmail.com>
2019-11-21 20:37:01 +0700

    Update allprofile.php

--- allprofile.php
+++ allprofile.php
@@ -4,7 +4,10 @@ $sql="select * from register";
 $result=mysqli_query($connect, $sql);
 if($result){
     while($count=mysqli_fetch_array($result,MYSQLI_ASSOC)){
-        
+        echo "Student id:".$count["userid"]."<br>";
+        echo "Name:".$count["Name"]."<br>";
+        echo "Surname:".$count["Surname"]."<br>";
+        echo "E-mail:".$count["email"]."<br>";
     }
 } else{
 }
