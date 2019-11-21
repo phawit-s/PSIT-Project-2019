4664cd393e7981fb73a28dd77eb19cb18b9a2d36
phawit-s <phawit.ps@gmail.com>
2019-11-21 20:34:23 +0700

    Update allprofile.php

--- allprofile.php
+++ allprofile.php
@@ -1,1 +1,10 @@
 <?php
+require 'connect.php';
+$sql="select * from register";
+$result=mysqli_query($connect, $sql);
+if($result){
+    while($count=mysqli_fetch_array($result,MYSQLI_ASSOC)){
+        
+    }
+} else{
+}
