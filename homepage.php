6db9f086a5c6c7467d8b0dd61429304ce913ae5c
phawit-s <phawit.ps@gmail.com>
2019-11-23 10:00:28 +0700

    Update homepage.php

--- homepage.php
+++ homepage.php
@@ -15,6 +15,7 @@
           <li><a href="logout.php">Logout</a></li>
           <li><a href="profile.php">Welcome <?php echo $_SESSION['Name']; ?> <?php echo $_SESSION['Surname']; ?></a></li>
           <li><a href="contact page.php">Contact Us</a></li>
+          
         <?php }else { ?>
                 <li><a href="login.html">Login</a></li>
                 <li><a href="register.html">Register</a></li>
@@ -28,21 +29,20 @@
     </div>
     <div>
         <div class="textbox2">
-          <b id="txt2">
-            <p id="txt2_h">Need a Tutor ???<br><b style="font-size: 20px; margin-bottom: 10px;">"you need to go to random page"</b></p>
-            <p id="txt_3"><a href="" style="text-decoration: none; color: white ;text-align: center; padding-left: 60px;">Go To Random Page</a></p>
-          </b>
           <?php if(isset($_SESSION['Name'])) { ?>
-          
-            
-          <?php }else { ?>
             <b id="txt3">
             <p id="already">
             Already Have A Tutor ???
             </p>
             <p id="already2">!!!Please click this !!!</p><br><br>
-            <p><a href="" id="login_in_home">Go To Chat Page Nowww</a></p>
+            <p><a href="random.php" id="login_in_home">Go To Chat Page Nowww</a></p>
           </b>
+          <?php }else { ?>
+              <b id="txt2">
+            <p id="txt2_h">Need a Tutor ???<br><b style="font-size: 20px; margin-bottom: 10px;">"you need to go to register first!!"</b></p>
+            <p id="txt_3"><a href="login.html" style="text-decoration: none; color: white ;text-align: center; padding-left: 60px;">Plese Login!!!!</a></p>
+          </b>
+            
           <?php } ?>
         </div>
     </div>
