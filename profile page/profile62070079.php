cf5b04942c11ae3b01aed68344d65c0f23b60823
Tuangtnp <56257346+Tuangtnp@users.noreply.github.com>
2019-12-15 17:09:44 +0700

    เปลี่ยนพื้นหลัง

--- profile page/profile62070079.php
+++ profile page/profile62070079.php
@@ -21,6 +21,14 @@ $result=mysqli_query($con,$sql);
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 </head>
+<style>
+  body {
+    background-image: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url("https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS4AwmPRQ2VzUlINwmC1wAPujk8wtV2qoRZsxDABzwP657IYkYJ");
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: cover;
+  }
+</style>
 <body background="https://images.unsplash.com/photo-1556761175-5973dc0f32e7?ixlib=rb-1.2.1&auto=format&fit=crop&w=1490&q=80">
     <center>
       	<!--NavBar Section-->
